90dd407e545bf1b14fe710a0d07e189dc60658f4
Edward_MIT <usmanoff19980404@gmail.com>
2025-11-20 22:40:27 +0900

    feat: function arguments

---
+++ function_arg.py
@@ -0,0 +1,14 @@
+# malumotlar funksiyaga argument sifatida uzatioa oladi
+
+# a function with one argument
+
+def my_function(fname):
+  print(fname + "Refsnes")
+
+# Parametr - bu function ta'rifida (ya'ni function yaratishda) qavs ichida yozilgan o'zgaruvchi
+
+
+def my_functions(name):  # name is a parametr
+  print("hello", name)
+
+print(my_functions("Abduqahhor"))
